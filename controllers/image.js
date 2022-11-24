eed4084d718cb3c10cde332b0b56b727a1e5cb83
Runwiththewolf93 <stevan38a@gmail.com>
2022-11-24 17:47:37 +0100

    fix bug

--- controllers/image.js
+++ controllers/image.js
@@ -1,10 +1,9 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+  apiKey: "YOUR_API_KEY_HERE",
 });
-
 
 const handleApiCall = (req, res) => {
   app.models
@@ -21,25 +20,26 @@ const handleApiCall = (req, res) => {
     .then(data => {
       res.json(data);
     })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
+    .catch(err => res.status(400).json("unable to work with API"));
+};
 
 const handleImage = (req, res, db) => {
   const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-    // entries[0] --> this used to return the entries
-    // TO
-    // entries[0].entries --> this now returns the entries
-    res.json(entries[0].entries);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-}
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then(entries => {
+      // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
+      // entries[0] --> this used to return the entries
+      // TO
+      // entries[0].entries --> this now returns the entries
+      res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json("unable to get entries"));
+};
 
 module.exports = {
   handleImage,
-  handleApiCall
-}
+  handleApiCall,
+};
